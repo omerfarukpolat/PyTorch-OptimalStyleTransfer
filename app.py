c6ffcb1e68e830f2c12b322b93cbffcc814dc038
omerfarukpolat <omerfarukpolat@etu.edu.tr>
2024-06-02 22:08:21 +0300

    add an appendix for avoiding same file error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file, jsonify
 import subprocess
 import os
 import time
@@ -14,6 +14,18 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(RESULTS_FOLDER, exist_ok=True)
 os.makedirs(STYLE_FOLDER, exist_ok=True)
 
+def save_unique_file(directory, filename):
+    """
+    Save a file with a unique name in the specified directory.
+    If the file already exists, append a counter to the filename.
+    """
+    base, ext = os.path.splitext(filename)
+    counter = 1
+    unique_filename = filename
+    while os.path.exists(os.path.join(directory, unique_filename)):
+        unique_filename = f"{base}_{counter}{ext}"
+        counter += 1
+    return unique_filename
 
 @app.route('/transfer', methods=['POST'])
 def transfer():
@@ -27,7 +39,7 @@ def transfer():
 
     alpha = request.form.get('alpha', 1.0)
 
-    content_filename = content_image.filename
+    content_filename = save_unique_file(UPLOAD_FOLDER, content_image.filename)
     content_path = os.path.join(UPLOAD_FOLDER, content_filename)
     style_path = os.path.join(STYLE_FOLDER, f'{style_name}.jpg')
 
@@ -52,12 +64,7 @@ def transfer():
 
     print(f"Running time: {running_time} seconds")
 
-    response = {
-        'image': result_path
-    }
-
-    return jsonify(response)
-
+    return send_file(result_path, mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
